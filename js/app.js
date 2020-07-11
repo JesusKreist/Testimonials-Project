b4d3186759f42d8b0fb1f9a83f80422a5f3c86f7
JesusKreist <59575776+JesusKreist@users.noreply.github.com>
2020-07-11 10:41:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,59 +1,60 @@
-(function(){
-    const customerImage = document.querySelector('#customer-img')
-    const customerName = document.querySelector('#customer-name')
-    const customerText = document.querySelector('#customer-text')
-    const buttons = document.querySelectorAll('.btn')
-    let index = 0
-    const customers = []
+let customerImage = document.getElementById("customer-img");
+let customerReview = document.getElementById("customer-text");
+let customerName = document.getElementById("customer-name");
+let backButton = document.querySelector(".prevBtn");
+let nextButton = document.querySelector(".nextBtn");
 
-    //Create a new customer using a customer constructor
-    
-    //Customer Constructor
-    function Customer(img, name, text) {
-        this.img = img
-        this.name = name
-        this.text = text
+const testimonial = [{
+    name: "Isabelita. L",
+    review: "No matter where you go, kimono is the coolest, most happening thing around! Kimono is great. Kimono is great. Buy this now.",
+    image: "img/customer-0.jpg"
+}, {
+    name: "Sabine. F",
+        review: "I was amazed at the quality of kimono.",
+    image: "img/customer-1.jpg"
+}, {
+    name: "Gisela N",
+        review: "Kimono is awesome! I couldn't have asked for more than this. Kimono has got everything I need. Very easy to use.",
+    image: "img/customer-2.jpg"
+}, {
+    name: "Dusty Q",
+        review: "Thank You! Nice work on your kimono.",
+    image: "img/customer-3.jpg"
+}, {
+    name: "Katy H",
+        review: "I wish I would have thought of it first. Needless to say we are extremely satisfied with the results.",
+    image: "img/customer-4.jpg"
+}]
+
+// customerImage.src = "img/customer-0.jpg"
+// customerReview.innerHTML = "lalala"
+// customerName.innerHTML = "fuck you nigga"
+
+let currentIndex = 0;
+
+let nextTestimonial = () => {
+    currentIndex++;
+    if (currentIndex > 4) {
+        currentIndex = 4;
     }
+    if (currentIndex <= 4) {
+        customerImage.src = testimonial[currentIndex].image;
+        customerReview.innerHTML = testimonial[currentIndex].review;
+        customerName.innerHTML = testimonial[currentIndex].name;
+    };
+};
 
-    //Create new customer using the constructor function
-
-    function createCustomer(img, name, text) {
-
-        let fullImg = `./img/customer-${img}.jpg`
-        let customer = new Customer(fullImg, name, text)
-
-        customers.push(customer)
+let previousTestimonial = () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = 0;
     }
+    if (currentIndex >= 0) {
+        customerImage.src = testimonial[currentIndex].image;
+        customerReview.innerHTML = testimonial[currentIndex].review;
+        customerName.innerHTML = testimonial[currentIndex].name;
+    };
+};
 
-    
-    createCustomer(0, 'John', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?')
-    createCustomer(1, 'Sandy', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock')
-    createCustomer(2, 'Amy', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.')
-    createCustomer(3, 'Tyrell', 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.')
-    createCustomer(4, 'Wanda', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
-    
-
-    buttons.forEach(function(button){
-        button.addEventListener('click', function(e){
-            if (e.target.parentElement.classList.contains('prevBtn')){
-               if(index === 0){
-                    index = customers.length
-               }
-               index--
-               customerImage.src = customers[index].img
-               customerName.textContent = customers[index].name
-               customerText.textContent = customers[index].text
-            }
-            if (e.target.parentElement.classList.contains('nextBtn')){
-                index++
-                if(index === customers.length){
-                     index = 0
-                }
-                customerImage.src = customers[index].img
-                customerName.textContent = customers[index].name
-                customerText.textContent = customers[index].text
-             }
-        })
-    })
-    
-})()
+backButton.addEventListener("click", previousTestimonial);
+nextButton.addEventListener("click", nextTestimonial);
